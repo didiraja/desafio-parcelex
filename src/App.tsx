da8dc0dca56d2455abef940d8650a6257affc57d
Dico Didiraja <didiraja@didinote>
2023-10-20 00:10:06 -0300

    feat: 🎸 fix TS; Success screen;

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { DetailedHTMLProps, InputHTMLAttributes, useEffect, useState } from 'react'
 import { useForm, SubmitHandler } from "react-hook-form"
 import './App.css'
 
@@ -11,6 +11,9 @@ type Inputs = {
 }
 
 function App() {
+
+  const [isSuccess, setSuccess] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -18,9 +21,16 @@ function App() {
     formState: { errors },
   } = useForm<Inputs>()
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    // console.log(data)
 
-  const handleImage = (event) => {
+    setSuccess(true);
+  }
+
+  const handleImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+    if (!event.target.files) return;
+
     const imageURL = URL.createObjectURL(event.target.files[0]);
     setAvatar(imageURL);
   }
@@ -32,119 +42,139 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="card-wrapper">
+        {
+          isSuccess ?
+          <div className="success-wrapper">
           <div className="card">
+            <h2 className="title">Obrigado!</h2>
 
-            <div className="grid grid-cols-1">
-              <div className="field avatar">
-                <p className="label">Avatar</p>
-                <img className="" src={avatarSrc} alt="" />
-              </div>
-            </div>
-
-            <div className="grid grid-cols-1 gap-2">
-              <div className="field name">
-                <p className="label">Nome</p>
-                <p>{watch("name")}</p>
-              </div>
-              <div className="field age">
-                <p className="label">Idade</p>
-                <p>{watch("age")}</p>
-              </div>
-            </div>
-
-            <div className="grid grid-cols-1 gap-2">
-              <div className="field phone">
-                <p className="label">Telefone</p>
-                <p>{watch("phone")}</p>
-              </div>
-              <div className="field email">
-                <p className="label">E-mail</p>
-                <p>{watch("email")}</p>
-              </div>
-            </div>
+            <p className="message">
+              Seu registro foi concluído com sucesso!
+            </p>
           </div>
         </div>
+          : null
+        }
 
-        <div className="form-wrapper">
-          <h2 className="title">Cadastro Parcelex</h2>
-          <div className="form">
-            <form className="" onSubmit={handleSubmit(onSubmit)}>
+        {
+          !isSuccess ? 
+            <>
+              <div className="card-wrapper">
+                <div className="card">
 
-              <div className="section">
-                <label className='label' htmlFor="avatar">Escolha seu avatar:</label>
-                <input
-                  name='avatar'
-                  className="field"
-                  type="file"
-                  onChange={handleImage}
-                  required
-                />
-                {errors.avatar && <span className='error-text'>{ERROR_MSG}</span>}
+                  <div className="grid grid-cols-1">
+                    <div className="field avatar">
+                      <p className="label">Avatar</p>
+                      <img className="" src={avatarSrc} alt="" />
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                    <div className="field name">
+                      <p className="label">Nome</p>
+                      <p>{watch("name")}</p>
+                    </div>
+                    <div className="field age">
+                      <p className="label">Idade</p>
+                      <p>{watch("age")}</p>
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                    <div className="field phone">
+                      <p className="label">Telefone</p>
+                      <p>{watch("phone")}</p>
+                    </div>
+                    <div className="field email">
+                      <p className="label">E-mail</p>
+                      <p>{watch("email")}</p>
+                    </div>
+                  </div>
+                </div>
               </div>
 
-              <div className="section">
-                <label className='label' htmlFor="name">Preencha seu nome:</label>
-                <input
-                  className="field"
-                  type="text"
-                  placeholder="João da Silva"
-                  maxLength={20}
-                  pattern="[A-Za-z]+"
-                  {...register("name", {
-                    required: true
-                  })}
-                />
-                {errors.name && <span className='error-text'>{ERROR_MSG}</span>}
+              <div className="form-wrapper">
+                <h2 className="title">Cadastro Parcelex</h2>
+                <div className="form">
+                  <form className="" onSubmit={handleSubmit(onSubmit)}>
+
+                    <div className="section">
+                      <label className='label' htmlFor="avatar">Escolha seu avatar:</label>
+                      <input
+                        name='avatar'
+                        className="field"
+                        type="file"
+                        onChange={handleImage}
+                        required
+                      />
+                      {errors.avatar && <span className='error-text'>{ERROR_MSG}</span>}
+                    </div>
+
+                    <div className="section">
+                      <label className='label' htmlFor="name">Preencha seu nome:</label>
+                      <input
+                        className="field"
+                        type="text"
+                        placeholder="João da Silva"
+                        maxLength={20}
+                        pattern="[A-Za-z]+"
+                        {...register("name", {
+                          required: true
+                        })}
+                      />
+                      {errors.name && <span className='error-text'>{ERROR_MSG}</span>}
+                    </div>
+
+                    <div className="section">
+                      <label className='label' htmlFor="Idade">Preencha sua idade:</label>
+                      <input
+                        className="field"
+                        type="number"
+                        placeholder='99'
+                        min={18}
+                        max={99}
+                        {...register("age", {
+                          required: true,
+                        })}
+                      />
+                      {errors.age && <span className='error-text'>{ERROR_MSG}</span>}
+                    </div>
+
+                    <div className="section">
+                      <label className='label' htmlFor="número">Preencha seu número:</label>
+                      <input
+                        className="field"
+                        type="number"
+                        placeholder='99999-9999'
+                        min={11111111}
+                        max={999999999}
+                        {...register("phone", {
+                          required: true,
+                        })}
+                      />
+                      {errors.phone && <span className='error-text'>{ERROR_MSG}</span>}
+                    </div>
+
+                    <div className="section">
+                      <label className='label' htmlFor="email">Preencha seu email:</label>
+                      <input
+                        className="field"
+                        type="email"
+                        placeholder='joao@dasilva.com'
+                        maxLength={30}
+                        {...register("email", { required: true })}
+                      />
+                      {errors.email && <span className='error-text'>{ERROR_MSG}</span>}
+                    </div>
+
+                    <input className='btn-submit' type='submit' value="Enviar" />
+                  </form>
+
+                </div>
               </div>
-
-              <div className="section">
-                <label className='label' htmlFor="número">Preencha seu número:</label>
-                <input
-                  className="field"
-                  type="number"
-                  placeholder='99999-9999'
-                  min={11111111}
-                  max={999999999}
-                  {...register("phone", {
-                    required: true,
-                  })}
-                />
-                {errors.phone && <span className='error-text'>{ERROR_MSG}</span>}
-              </div>
-
-              <div className="section">
-                <label className='label' htmlFor="email">Preencha seu email:</label>
-                <input
-                  className="field"
-                  type="email"
-                  placeholder='joao@dasilva.com'
-                  maxLength={30}
-                  {...register("email", { required: true })}
-                />
-                {errors.email && <span className='error-text'>{ERROR_MSG}</span>}
-              </div>
-
-              <div className="section">
-                <label className='label' htmlFor="Idade">Preencha sua idade:</label>
-                <input
-                  className="field"
-                  type="number"
-                  placeholder='99'
-                  min={18}
-                  max={99}
-                  {...register("age", {
-                    required: true,
-                  })}
-                />
-                {errors.age && <span className='error-text'>{ERROR_MSG}</span>}
-              </div>
-
-              <input className='btn-submit' type='submit' value="Enviar" />
-            </form>
-
-          </div>
-        </div>
+            </> 
+          : null
+        }
       </div>
     </>
   )
